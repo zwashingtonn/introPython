f31fae23f63ba8472c8b24a1822b647451edaf2f
zwashingtonn <144712436+zwashingtonn@users.noreply.github.com>
2023-09-14 16:57:18 +0000

    first commit

---
+++ quizqs/quiz.py
@@ -0,0 +1,41 @@
+# 1. In your on words, describe what a variable is.
+a varible is like a closet for data values. 
+# 2. Name three (3) operators we've learned about in class and describe 
+operateros we learned about in class arithmatics addition, subtraction, and multiplication 1 is a+b , 2 is a-b 3 a*b
+# a situation where you would use each one.  
+1. i have 2 bunnies a and 3 cats b, whats the total of how many animals I have. 
+2. I have 1 aariz and 2 josish, what is 
+# 3. Describe the difference between camal case and pascal case.
+camel case is the second word of a varible name, every word after starts with a capital letter.
+# 4. Which operator is being used in the example below
+numberOfApples = 10
+numberOfOranges = 12
+
+numberOfApples < numberOfOranges
+Its a comparsion operater 
+# 5. You have been given a $100.00 dollar gift card from a family member. Another 
+# family member adds 50.00 dollars to your gift card and a friend gives you $35.50. 
+# Your task is to create variables for each gift amount and a final variable that
+# represents the total amount of money on your gift card. 
+The final varible would be a float?
+
+# 6. Create a variable that uses the input function and then print that variable. Your variable 
+# should ask a user to enter their name.  
+name = input ("what is your full name")
+# 7. You have been asked to create a program for a community center that wants to plan a cookout. 
+# The community center wants you to build a form program and capture several key data points. 
+# They want to know a visitor's full name, their age, if they have an allergy, and what their 
+# favorite food is. You need to create the appropriate varialbes with the appropriate data type
+# and use the input() function and print function to take in and print out the user's responses.
+ name = input("what is your name")
+age = input("what is your age")
+
+# 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
+# wants to be able to know if a user has a gluten allegery before they make the sandwich order.
+# If the user DOES have a glutten allergy, they need to eat a special type of bread, if the user DOES NOT
+# have an allergy they can eat the regular bread. What data type would best be used in this 
+# scenario and how would you write a variable to represent this value? A data type that can be used is boolen because it can ask someone if they are allergic to grass true/false.
+
+# 9. In your own words, describe what a program is?
+A program is a specific set of ordered operations. 
+Describe the difference between a float and an integer. a float is a number with a decimal and a integer is any whole number 
