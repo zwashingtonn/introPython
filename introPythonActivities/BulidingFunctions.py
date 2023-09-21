6efc6411054f843bf50b087f7b1bfdad4810d57f
zwashingtonn <144712436+zwashingtonn@users.noreply.github.com>
2023-09-21 16:28:49 +0000

    buliding functions

---
+++ introPythonActivities/BulidingFunctions.py
@@ -0,0 +1,11 @@
+#number= False
+#print(type(number))
+
+#numberOfBooks = 3490909
+#print(float(numberOfBooks))
+
+#numberOfPencils = 1242.023343
+#print(int(numberOfPencils))
+
+fraction_of_pizza ='.9999999999'
+print(type(container))
