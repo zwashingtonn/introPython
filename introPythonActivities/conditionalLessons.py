de70e3a74bb5652872184f04af5cb02b3d35be3e
zwashingtonn <144712436+zwashingtonn@users.noreply.github.com>
2023-09-27 17:04:38 +0000

    IMMMM DONEEE!!

---
+++ introPythonActivities/conditionalLessons.py
@@ -0,0 +1,42 @@
+# 1. What is the difference between 
+# a parameter and an argument in a python function
+"A paramater is a data holder while an arguement is the data."
+
+# 2. In your own words, describe what 
+# a conditional statement (if/else) is
+if is if it is correct u will get a certain text but if it is anything else(else) it will be incorrect.
+
+# 3. write a simple conditional state using a comparision operator(greater than, less than, etc. )
+arianagrande money = 262.22
+costoflashes = 189
+
+# 4. Write a conditional statement for food in the refridgerator.
+# your conditional statement should be wrapped in a function that takes one (1)
+# parameter. The data type for this parameter should be true or false. 
+# your function should have a line of code that asks the user if there is food in the refridgerator.
+# If there is food in the refridgerator, print time to cook. If there is 
+# NOT food in the fridge, print time to shop. 
+# When you call your function there should be an argument that accepts 
+# a boolean.
+foodinfrige = 5
+foodneeded = 2
+def foodinfrige():
+    foodinfrige = False
+    if foodinfrige == true:
+     print (' time to cook')
+else print (' no food!')
+
+or 
+
+
+def frige():
+    foodinfrige = input ('food in frige') 
+    if foodinfrige == 'cook for 5 minutes'
+      print('ok food is cooking for 5 minutes')
+    else: 
+       print(' Sorry there is no food in the frige')
+# 5. Create a function that checks the inventory of cereal for a store. 
+# your function should have a parameter that accepts an integer. In your function
+# use a conditional statement to determine if you need to order more cereal.
+# If there is more than 10 boxes, print "inventory full", else if there are less than 
+# 10 boxes print "we need to order more cereal".
